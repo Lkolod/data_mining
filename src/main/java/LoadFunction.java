03b467fba6463edd5385f1b5676546978872beeb
Lkolod <kolodziej051@gmail.com>
2024-03-14 15:14:14 +0100

    moviers rating part2

---
+++ src/main/java/LoadFunction.java
@@ -0,0 +1,102 @@
+import com.github.sh0nk.matplotlib4j.Plot;
+import com.github.sh0nk.matplotlib4j.PythonExecutionException;
+import org.apache.spark.ml.feature.VectorAssembler;
+import org.apache.spark.ml.linalg.Vectors;
+import org.apache.spark.ml.regression.LinearRegression;
+import org.apache.spark.ml.regression.LinearRegressionModel;
+import org.apache.spark.ml.regression.LinearRegressionTrainingSummary;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Encoders;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class LoadFunction {
+    public static void main(String[] args) {
+        SparkSession spark = SparkSession.builder()
+                .appName("LoadFunction")
+                .master("local")
+                .getOrCreate();
+        System.out.println("Using Apache Spark v" + spark.version());
+
+        StructType schema = DataTypes.createStructType(new StructField[] {
+                DataTypes.createStructField(
+                        "X",
+                        DataTypes.DoubleType,
+                        true),
+                DataTypes.createStructField(
+                        "Y",
+                        DataTypes.DoubleType,
+                        true),
+                  });
+
+        Dataset<Row> df = spark.read()
+                .format("csv")
+                .option("header", "true")
+                .schema(schema)
+                .load("data/xy-001.csv");
+
+
+        VectorAssembler assembler = new VectorAssembler()
+                .setInputCols(new String[]{"X"})
+                .setOutputCol("features");
+
+        Dataset<Row> df_transformed = assembler.transform(df);
+        //task 1 end
+        //df_transformed.show();
+
+
+        //task 1.2
+        //task 1.2.1
+        LinearRegression lr = new LinearRegression()
+                .setMaxIter(10)
+                .setRegParam(0.3)
+                .setElasticNetParam(0.8)
+                .setFeaturesCol("features")
+                .setLabelCol("Y");
+
+        // Fit the model.
+        LinearRegressionModel lrModel = lr.fit(df_transformed);
+        //task 1.2.2
+        System.out.println(lrModel.coefficients());
+        System.out.println(lrModel.intercept());
+
+        //task 1.2.3
+        LinearRegressionTrainingSummary trainingSummary = lrModel.summary();
+        System.out.println("numIterations: " + trainingSummary.totalIterations());
+        System.out.println("objectiveHistory: " + Vectors.dense(trainingSummary.objectiveHistory()));
+        trainingSummary.residuals().show(100);
+        System.out.println("MSE: " + trainingSummary.meanSquaredError());
+        System.out.println("RMSE: " + trainingSummary.rootMeanSquaredError());
+        System.out.println("MAE: " + trainingSummary.meanAbsoluteError());
+        System.out.println("r2: " + trainingSummary.r2());
+
+
+        var summarry = trainingSummary.objectiveHistory();
+        summarry.cast(D);
+    }
+    static void plotObjectiveHistory(List<Double> lossHistory){
+        var x = IntStream.range(0,lossHistory.size()).mapToDouble(d->d).boxed().toList();
+        Plot plt = Plot.create();
+        plt.plot().add(x, lossHistory).label("loss");
+        plt.xlabel("Iteration");
+        plt.ylabel("Loss");
+        plt.title("Loss history");
+        plt.legend();
+        try {
+            plt.show();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (PythonExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        MoviesRatings.main(null);
+        LoadFunction.main(null);
     }
 }
